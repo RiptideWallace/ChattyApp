7cf22b419a015b898d0bc9832b04b5cacc2042c2
ChrisBryson <brysonc1987@gmail.com>
2017-08-01 11:44:03 -0700

    Comments added for clarity

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,8 +5,9 @@ class Message extends Component {
   render() {
 
     return (
-     <div className="message">
-      <span className="message-time" title={this.props.timestamp.day}>{this.props.timestamp.time}</span>
+      // Space where the username and message are displayed when inputted
+      // User colors are applied here
+     <div className="message">  
       <span className="message-username" style={{color: this.props.color}}>{this.props.username}</span>
       <span className="message-content">{this.props.content}</span>
     </div>
