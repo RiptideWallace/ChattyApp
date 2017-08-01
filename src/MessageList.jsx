39a468640f1dc94400554550b2b6ea998ce068d8
ChrisBryson <brysonc1987@gmail.com>
2017-08-01 11:46:27 -0700

    Comments added for clairty & timestamp removed until fully functional

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,14 +4,14 @@ import Message from './Message.jsx';
 class MessageList extends Component {
 
   render() {
-
+    // This code does a loop through all the messages and applies the below keys to them
+    // If a message is recognized as a notification, the styling of "message system"(italics) is applied
     const messages = this.props.messages.map(message => {
       if(message.type === 'incomingNotification') {
         return (<div className="message system" key={message.id}>{message.content}</div>)
       } else {
         return (<Message
           key={ message.id }
-          timestamp={message.timestamp}
           color={message.color}
           username ={ message.username }
           content={ message.content } />)
