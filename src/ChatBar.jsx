33515f798073ece06df02db37706fc4812517886
ChrisBryson <brysonc1987@gmail.com>
2017-08-01 11:40:42 -0700

    Comments added for clarity

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -7,14 +7,16 @@ class ChatBar extends Component {
     this.sendMessageOnEnter = this.sendMessageOnEnter.bind(this);
     this.changeUserOnEnter = this.changeUserOnEnter.bind(this);
   }
-
+   // The action that is taken when the Enter key is pressed in the message box
+   // Inputs whatever is typed in the message box to the app
   sendMessageOnEnter(input){
     if (input.key === 'Enter') {
       this.props.newMessage({name: this.props.currentUser.name, content: input.target.value});
       input.target.value = "";
     }
   }
-
+   // The action that is taken when a new user is added or if a username is changed
+   // When enter is pressed on the user box, the name typed in will be that users new name
   changeUserOnEnter(input) {
     if (input.key === 'Enter') {
       this.props.newUser(input.target.value);
@@ -22,6 +24,7 @@ class ChatBar extends Component {
   }
   render() {
     return (
+       //Applies the above functions into the app when the input boxes are interacted with
       <footer className="chatbar">
         <input className="chatbar-username" placeholder="Type in your name and hit ENTER" onKeyPress={this.changeUserOnEnter} />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.sendMessageOnEnter}/>
