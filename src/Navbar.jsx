448d2b42f034405fd714056b15dbf428a7e8cad9
ChrisBryson <brysonc1987@gmail.com>
2017-08-01 11:47:30 -0700

    Comments added for clarity

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 class Navbar extends Component {
   render() {
     return (
+      // This where the title of the app and users online is displayed
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
          <span className='user-count'>{this.props.count} Users Online </span>
