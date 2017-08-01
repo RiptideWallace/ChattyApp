20995a861393b3d8b59e3cd0e64ab91faf71a691
ChrisBryson <brysonc1987@gmail.com>
2017-08-01 11:37:37 -0700

    Commenting added for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import MessageList from './MessageList.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
-
+    // Initial app information that will hold who the currentUser is, what messages have been sent 
+    // and the amount of users online
     this.state = {
       currentUser: [],
       messages: [],
@@ -29,35 +30,37 @@ class App extends Component {
     websocket.onmessage = (event) => {
       const data = JSON.parse(event.data);
       switch(data.type) {
-
+        //Recieves an incoming message from the server to be displayed in the app
         case 'incomingMessage':
           const messages = this.state.messages.concat(data);
           this.setState({messages: messages});
         break;
-
+        //Recieves an incoming notification from the server to be displayed in the app
         case 'incomingNotification':
           const notifications = this.state.messages.concat(data);
           this.setState({messages: notifications});
         break;
-
+        // Sets the state of the user count when the connection between the app and the server opens
         case 'clientCount':
           this.setState({count: data.count});
         break;
-
+        // Sets the state of the user color when the connection between the app and the server opens
         case 'setUserColor':
           this.setState({color: data.color});
         break;
-
+      // Errors are thrown if anything goes wrong on the opening of the websocket
       default:
         throw new Error('Unknown event type' + data.type);
       }
     }
   }
+  //Sends a new message to the server when it is created and inputed into the app
   newMessage(msg) {
     const newMessage = {type: 'postMessage', id: msg.id, username: msg.name, content: msg.content};
     this.socket.send(JSON.stringify(newMessage));
   }
-
+  //Sends a new user to the server when they are created and inputed into the app
+  //Renders the notification that is displayed in the app when a new user is created
   newUser(username) {
     const notification = {type: 'postNotification', content: this.state.currentUser.name + ' has changed their name to ' + username}
     this.socket.send(JSON.stringify(notification));
@@ -66,6 +69,8 @@ class App extends Component {
 
   render() {
     return (
+      //Creates a connection between the App page and all the other pages and every function 
+      // that interactes between the two
       <div>
         <Navbar count = {this.state.count}/>
         <MessageList  notification = {this.state.notification}
